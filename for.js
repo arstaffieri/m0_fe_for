22296d1e62719d8d321c4cfcb523138126df28ec
Angie Staffieri <angie.staffieri@gmail.com>
2022-06-10 19:30:43 -0600

    I did my best

--- for.js
+++ for.js
@@ -1,22 +1,34 @@
 // As you complete each challenge, RUN THE CODE to ensure it works. You can do this by
 // running `node <filepath>`. If you are in this directory, you'd run `node for.js`.
-  
+
 // Challenge 1
 // Use a for statement to log to the console all even numbers between 25 and 75.
-
+for(i=25; i<= 75; i++){
+  if(i % 2 == 0){
+    console.log(i);
+  }
+}
 
 
 // Challenge 2
 // Use a for statement to log to the console a complete sentence stating the weather using the Array below:
 var weather_options = ["sunny", "snowy", "freezing", "partly cloudy", "drizzly"]
-
+for (var i = 0; i < weather_options.length; i++){
+  console.log( "I love when the weather is " + weather_options[i]);
+}
 
 // Challenge 3
 // Use a for statement to log to the console only the words that are exactly 3 characters in length
 var words = ["car", "bike", "bus", "van", "scooter", "truck", "rv"]
-
-// 📹 Record yourself live-coding and talking through any ONE of the challenges. 
-  // The recording doesn't have to be the first time you are solving the problem. 
-  // Use technical vocabulary and explain each part to demonstrate your understanding! 
+for (var i = 3; i = words.length;){
+  console.log(words[i]);
+}
+// This doesn't work and creates an infinate loop that crashes my terminal
+//when I try to run it. Unfortunately, everyone else has stopped working for today
+//and the study room is closed. I cannot figure out what I am doing wrong here.
+// I am going to ask as soon as I can find someone who can answer me but, for now,
+//I know it's wrong.
+// 📹 Record yourself live-coding and talking through any ONE of the challenges.
+  // The recording doesn't have to be the first time you are solving the problem.
+  // Use technical vocabulary and explain each part to demonstrate your understanding!
   // Share the link to your video in your small group channel.
-
--- selfEvaluation.md
+++ selfEvaluation.md
@@ -1,6 +1,10 @@
 # Self Evaluation
 
 - How confident do you feel in your understanding and fluency with the `for` statement?
+I thought I was very confident before the third challenge and now I wonder if I understand any of it at all. This was an awful lot and my brain is completely fried.
 - This was a much more advanced concept than anything you've done in Mod 0, and the exercises required you to push yourself to apply other background knowledge or learn things outside of the curriculum - how did that feel? Did you have productive struggle? Unproductive struggle?
+Challenge 3 started out as a productive struggle but has moved into an unproductive struggle and I need to step away from it with the plan to return to it this weekend. For the life of me, I cannot figure out what I am doing wrong and I am getting frustrated.
 - What do you still need to practice or learn? How will you do that? Do you need to adjust your calendar in order to do that?
+I need to go over all of the lessons again to make sure that I really understand what all the code is doing.
 - What questions do you still have? How will you get the answers you need?
+My question is why is my answer to Challenge 3 an infinite loop? I understand if I have some syntax wrong and I'd expect an error but it just keeps going "undefined" until the thing crashes. I'm going to ask my team and another Turing grad for help with this. 
